9d76c8216bfb29dc0012b130167a8a1c447c22c9
Jager-yoo <y73447jh@gmail.com>
2023-02-11 17:44:49 +0900

    Initial Commit

---
+++ TextMaster/TextMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextMasterApp.swift
+//  TextMaster
+//
+//  Created by 유재호 on 2023/02/11.
+//
+
+import SwiftUI
+
+@main
+struct TextMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
